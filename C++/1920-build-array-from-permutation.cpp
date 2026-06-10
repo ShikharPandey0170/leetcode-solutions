b2d95703980773c8f3b0068c30025300bfd920a9
Shikhar Pandey <shikharpandey0170@gmail.com>
2026-06-10 16:35:20 +0530

    Create 1920-build-array-from-permutation.cpp

---
+++ C++/1920-build-array-from-permutation.cpp
@@ -0,0 +1,16 @@
+// LeetCode 1920: Build Array from Permutation
+// Difficulty: Easy
+// Topic: Array
+// Link: https://leetcode.com/problems/build-array-from-permutation/
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        for(int i=0; i<n ;i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+};
