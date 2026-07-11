db9bee61466e700ffb19c3117469fa393d5d2026
Shikhar Pandey <shikharpandey0170@gmail.com>
2026-07-12 00:48:27 +0530

    Time: 2 ms (66.05%), Space: 17.7 MB (100%) - LeetHub

---
+++ Python/0043-multiply-strings.py
@@ -0,0 +1,17 @@
+# LeetCode 0043: Multiply Strings
+# Difficulty: Medium
+# Topic: Math
+# Link: https://leetcode.com/problems/multiply-strings/description/
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        str1,str2=0,0
+        for i in num1:
+            str1=str1*10+int(i)
+        for j in num2:
+            str2=str2*10+int(j)
+        str3=str(str1*str2)
+        return str3
+            
+      
+        
