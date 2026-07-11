46df15a9d02eed8d17f5ad9ee1473c48a00ba077
Shikhar Pandey <shikharpandey0170@gmail.com>
2026-07-12 00:47:37 +0530

    Time: 256 ms (98.8%), Space: 0B (100%) - LeetHub

--- SQL/0176-second-highest-salary.sql
+++ SQL/0176-second-highest-salary.sql
@@ -1,9 +1,8 @@
--- LeetCode 176: Second Highest Salary
--- Difficulty: Medium
--- Topic: Database
--- Link: https://leetcode.com/problems/second-highest-salary/description/
+# LeetCode 0176: Second Highest Salary
+# Difficulty: Medium
+# Topic: Database
+# Link: https://leetcode.com/problems/second-highest-salary/description/
 
-SELECT max(salary) 
-AS SecondHighestSalary  
+SELECT MAX(salary) AS SecondHighestSalary  
 FROM Employee 
-WHERE salary<(SELECT max(salary) FROM Employee);
+WHERE salary < (SELECT MAX(salary) FROM Employee);
